75f8fdccaea9a2538c1c4161d2073fdecee0aaee
Shraddha1222 <sharmashraddha041@gmail.com>
2024-05-23 16:26:56 +0000

    Add system_status.sh script

---
+++ system_status.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Display CPU activity level
+echo "CPU Activity Level:"
+uptime
+
+# Display free memory
+echo -e "\nFree Memory:"
+free -h
+
+# Display free disk space
+echo -e "\nFree Disk Space:"
+df -h
